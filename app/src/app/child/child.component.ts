6b182678189c66e2b79429634bdce9afe28547a3
iverimefarishvili <mefarishvili922@gmail.com>
2019-09-12 13:10:28 +0400

    add

--- app/src/app/child/child.component.ts
+++ app/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -9,14 +9,22 @@ export class ChildComponent implements OnInit {
   @Input() child;
   isActive = true;
   
+  @ViewChild("selector", {static: false}) divView: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
     console.log(this.child);
+    
   }
 
   public clickChild(name: any): void {
     name.isActive = !name.isActive;
+    if(name.isActive) {
+      this.divView.nativeElement.classList.add('openfolder');
+    } else {
+      this.divView.nativeElement.classList.remove('openfolder');
+    }
     console.log(name);
   }
 
